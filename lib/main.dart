99d308b8a43c1c035ade9ac92dccd7a4b8ae2318
mac2012 <mac2012@mac.com>
2022-05-26 19:57:09 +1000

    not working - pushed

--- lib/main.dart
+++ lib/main.dart
@@ -1,56 +1,53 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-void main() {
-  runApp(MyApp());
-}
-
-ThemeData _darkTheme = ThemeData(
-    brightness: Brightness.dark,
-    primaryColor: Colors.amber,
-    buttonTheme: const ButtonThemeData(
-      buttonColor: Colors.amber,
-      disabledColor: Colors.grey,
-    ));
+void main() => runApp(MyApp());
 
 ThemeData _lightTheme = ThemeData(
-    brightness: Brightness.light,
-    primaryColor: Colors.blue,
-    buttonTheme: const ButtonThemeData(
-      buttonColor: Colors.blue,
-      disabledColor: Colors.grey,
-    ));
+  brightness: Brightness.light,
+  primaryColor: Colors.blue,
+  buttonTheme: const ButtonThemeData(
+    buttonColor: Colors.blue,
+    disabledColor: Colors.grey,
+  ),
+);
+
+ThemeData _darkTheme = ThemeData(
+  brightness: Brightness.dark,
+  primaryColor: Colors.amber,
+  buttonTheme: const ButtonThemeData(
+    buttonColor: Colors.amber,
+    disabledColor: Colors.grey,
+  ),
+);
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      theme: _lightTheme,
-      darkTheme: _darkTheme,
-      debugShowCheckedModeBanner: false,
+      title: "Bottom Sheet",
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Dark Mode Demo'),
-        ),
-        body: Container(
-          child: Center(
+          appBar: AppBar(
+            title: Text("Theme Change"),
+            backgroundColor: Colors.green,
+            centerTitle: true,
+          ),
+          body: Center(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                Switch(
-                    value: true,
-                    onChanged: (val) {
-                      Get.changeThemeMode(
-                        Get.isDarkMode ? ThemeMode.light : ThemeMode.dark,
-                      );
-                    },
-                  )
+                ElevatedButton(
+                  child: Text("Change Theme"),
+                  onPressed: () {
+                    Get.changeThemeMode(
+                      Get.isDarkMode ? ThemeMode.light : ThemeMode.dark,
+                    );
+                  },
+                ),
               ],
             ),
-          ),
-        ),
-      ),
+          )),
     );
   }
 }
