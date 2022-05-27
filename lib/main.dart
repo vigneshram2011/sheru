d0cddb2d2fc99aea1d3733d440ac71564cbf4136
mac2012 <mac2012@mac.com>
2022-05-27 16:53:02 +1000

    working well - changes made

--- lib/main.dart
+++ lib/main.dart
@@ -1,53 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'home.dart';
 
 void main() => runApp(MyApp());
-
-ThemeData _lightTheme = ThemeData(
-  brightness: Brightness.light,
-  primaryColor: Colors.blue,
-  buttonTheme: const ButtonThemeData(
-    buttonColor: Colors.blue,
-    disabledColor: Colors.grey,
-  ),
-);
-
-ThemeData _darkTheme = ThemeData(
-  brightness: Brightness.dark,
-  primaryColor: Colors.amber,
-  buttonTheme: const ButtonThemeData(
-    buttonColor: Colors.amber,
-    disabledColor: Colors.grey,
-  ),
-);
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      title: "Bottom Sheet",
+      title: "Navigation",
       home: Scaffold(
-          appBar: AppBar(
-            title: Text("Theme Change"),
-            backgroundColor: Colors.green,
-            centerTitle: true,
+        appBar: AppBar(
+          title: Text("Navigation"),
+          backgroundColor: Colors.green,
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                child: Text("Go to Home"),
+                onPressed: () {
+                  Get.to(
+                    Home(),
+                    fullscreenDialog: true,
+                    transition: Transition.zoom,
+                  );
+                },
+              ),
+            ],
           ),
-          body: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                ElevatedButton(
-                  child: Text("Change Theme"),
-                  onPressed: () {
-                    Get.changeThemeMode(
-                      Get.isDarkMode ? ThemeMode.light : ThemeMode.dark,
-                    );
-                  },
-                ),
-              ],
-            ),
-          )),
+        ),
+      ),
     );
   }
 }
