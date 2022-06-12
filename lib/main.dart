86dd6ab180cd1efca2a142efd21bf1049490cfbe
mac2012 <mac2012@mac.com>
2022-06-12 10:27:48 +1000

    halfway through 21st video - getView completed

--- lib/main.dart
+++ lib/main.dart
@@ -4,82 +4,46 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'home.dart';
 import 'package:get_storage/get_storage.dart';
+import 'count_controller.dart';
 
-Future<void> main() async {
-  await GetStorage.init();
+void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  var emailEditingController = TextEditingController();
-  var storage = GetStorage();
+class MyApp extends GetView<CountController> {
   @override
   Widget build(BuildContext context) {
+    Get.put(CountController());
     return GetMaterialApp(
-      title: 'Get Storage & Email Validation',
+      title: 'Get View',
       home: Scaffold(
         backgroundColor: Colors.pink[50],
         appBar: AppBar(
           title: Text(
-            'Get Storage & Email Validation',
+            'Get View',
           ),
           backgroundColor: Colors.purple,
         ),
         body: Center(
-          child: Column(   
+          child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Padding(
-                padding: EdgeInsets.all(16.0),
-                child: TextField(
-                  controller: emailEditingController,
-                ),
-              ),
-              SizedBox(
-                height: 8,
-              ),
-              Padding(
-                padding: EdgeInsets.all(16.0),
-                child: ElevatedButton(
-                  child: Text('Write Email'),
-                  onPressed: () {
-                    if (GetUtils.isEmail(emailEditingController.text)) {
-                      storage.write('email', emailEditingController.text);
-                      Get.snackbar(
-                        'Success',
-                        'See Console for Email',
-                        backgroundColor: Colors.black,
-                        colorText: Colors.greenAccent,
-                        snackPosition: SnackPosition.TOP,
-                        forwardAnimationCurve: Curves.bounceInOut,
-                        reverseAnimationCurve: Curves.bounceInOut,
-                        animationDuration: Duration(seconds: 2),
-                        duration: Duration(seconds: 5),
-                      );
-                    } else {
-                      Get.snackbar(
-                        'Error',
-                        'Provide Email in Valid Format.',
-                        backgroundColor: Colors.yellowAccent,
-                        colorText: Colors.red,
-                        snackPosition: SnackPosition.TOP,
-                        forwardAnimationCurve: Curves.bounceInOut,
-                        reverseAnimationCurve: Curves.bounceInOut,
-                        animationDuration: Duration(seconds: 2),
-                        duration: Duration(seconds: 5),
-                      );
-                    }
-                  },
-                ),
-              ),
+              Obx(() => Text(
+                    'Value: ${controller.count}',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  )),
               SizedBox(
                 height: 8,
               ),
               ElevatedButton(
-                child: Text('Read Email'),
+                child: Text('Increment'),
                 onPressed: () {
-                  print('Email: ${storage.read('email')}');
+                  print(controller.hashCode);
+                  controller.increment();
                 },
               )
             ],
