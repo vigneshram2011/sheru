8d610b407703d2ed67cdfa88e3ebc6b9087b4dd4
mac2012 <mac2012@mac.com>
2022-06-13 11:51:52 +1000

    only alignment pending - everything is working

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -4,67 +4,90 @@ import 'package:get/get.dart';
 class Dashboard extends GetResponsiveView {
   @override
   Widget phone() {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'Sheru Mail',
+    if (GetPlatform.isIOS) {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'Sheru Mail',
+          ),
+          backgroundColor: Colors.red,
         ),
-        backgroundColor: Colors.red,
-      ),
-      body: Center(
-        child: Column(
-          children: [],
+        backgroundColor: Colors.pink,
+        body: Center(
+          child: Column(
+            children: <Widget>[
+              SizedBox(
+                height: 10,
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  labelText: 'Email',
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              ElevatedButton(
+                child: Text('Log In'),
+                onPressed: () {},
+              )
+            ],
+          ),
         ),
-      ),
-    );
-  }
-
-  @override
-  Widget tablet() {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'Sheru Mail',
+      );
+    }
+    if (GetPlatform.isAndroid) {
+      debugPrint('android called');
+      return Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'Sheru Mail',
+          ),
+          backgroundColor: Colors.red,
         ),
-        backgroundColor: Colors.red,
-      ),
-      backgroundColor: Colors.green,
-      body: Column(
-        children: [
-          Text(
-            'Hello World',
-            style: TextStyle(
-              fontSize: 60,
-              fontWeight: FontWeight.bold,
-              color: Colors.orange,
-            ),
+        backgroundColor: Colors.blue,
+        body: Center(
+          child: Column(
+            children: <Widget>[
+              SizedBox(
+                height: 10,
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  labelText: 'Email',
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              FloatingActionButton(
+                child: Text(
+                  'Log In',
+                ),
+                onPressed: () {},
+              )
+            ],
           ),
-          SizedBox(
-            height: 10,
-          ),
-          TextField(
-            decoration: InputDecoration(
-              labelText: 'Email',
-            ),
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          TextField(
-            decoration: InputDecoration(
-              labelText: 'Password',
-            ),
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          ElevatedButton(
-            child: Text('Log In'),
-            onPressed: () {},
-          ),
-        ],
-      ),
-    );
+        ),
+      );
+    }
+    return Container();
   }
 
   @override
@@ -80,6 +103,62 @@ class Dashboard extends GetResponsiveView {
       body: Center(
         child: Column(
           children: <Widget>[
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              'Email: ',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Email',
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              'Password: ',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Password',
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+              child: Text('Log In'),
+              onPressed: () {},
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget tablet() {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Sheru Mail',
+        ),
+        backgroundColor: Colors.red,
+      ),
+      backgroundColor: Colors.green,
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            Text('Hello World',
+                style: TextStyle(fontSize: 60, color: Colors.orange)),
             SizedBox(
               height: 10,
             ),
