c8930e57a1f639154ecac46676d6e9a2e52c2958
mac2012 <mac2012@mac.com>
2022-07-27 17:01:00 +1000

    changed starting position

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -24,7 +24,7 @@ class AppPages {
   AppPages._();
 
   // static const INITIAL = Routes.SPLASH;
-  static const INITIAL = Routes.CODE;
+  static const INITIAL = Routes.SPLASH;
 
   static final routes = [
     GetPage(
